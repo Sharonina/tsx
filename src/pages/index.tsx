9a84808e8c70f09a2ef63f7e7806899f9c552b1f
Sharon Arana <shar_mdza@hotmail.com>
2023-03-10 12:34:53 -0600

    añadiendo tipos a un hook

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import type { NextPage } from "next";
 import Head from "next/head";
 import { RandomFox } from "../components/RandomFox";
@@ -5,6 +6,11 @@ import { RandomFox } from "../components/RandomFox";
 const random = () => Math.floor(Math.random() * 123) + 1;
 
 const Home: NextPage = () => {
+  const [images, setImages] = useState<Array<string>>([
+    `https://randomfox.ca/images/${random()}.jpg`,
+    `https://randomfox.ca/images/${random()}.jpg`,
+    `https://randomfox.ca/images/${random()}.jpg`,
+  ]);
   return (
     <div>
       <Head>
@@ -14,11 +20,11 @@ const Home: NextPage = () => {
       </Head>
 
       <main>
-        <h1 className="text-3xl font-bold underline">Hello 😎!</h1>
-        <RandomFox
-          alt="random fox image"
-          image={`https://randomFox.ca/images/${random()}.jpg`}
-        />
+        {images.map((image, index) => (
+          <div className="p-4" key={index}>
+            <RandomFox image={image} />
+          </div>
+        ))}
       </main>
 
       <footer></footer>
