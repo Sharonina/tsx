83216b29b3a600ff3cf07dc560fc50d2c4e8be5b
Sharon Arana <shar_mdza@hotmail.com>
2023-03-13 21:08:01 -0600

    añadiendo lazy loading con observadores

--- src/components/RandomFox.tsx
+++ src/components/RandomFox.tsx
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 interface Props {
   image: string;
@@ -9,13 +9,39 @@ interface Props {
 export const RandomFox = ({ image, alt }: Props): JSX.Element => {
   const node = useRef<HTMLImageElement>(null); // se debe poner el elemento con el que se va a trabajar e inicializar en null cuando no sabemos el tipo
 
+  const [src, setSrc] = useState(
+    "data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzIwIiBoZWlnaHQ9IjMyMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB2ZXJzaW9uPSIxLjEiLz4="
+  );
+
+  useEffect(() => {
+    //nuevo observador
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        //on intersection > console.log
+        if (entry.isIntersecting) {
+          setSrc(image);
+        }
+      });
+    });
+
+    //observe node
+    if (node.current) {
+      observer.observe(node.current);
+    }
+
+    //desconect  - clean
+    return () => {
+      observer.disconnect();
+    };
+  }, [image]);
+
   return (
     <img
       ref={node}
       width={320}
       height="auto"
       className="mx-auto rounded-md bg-gray-300"
-      src={image}
+      src={src}
       alt={alt}
     />
   );
