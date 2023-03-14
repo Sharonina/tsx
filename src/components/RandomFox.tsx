bc4a3b55535f1b7264a3c06b269cb5bf52c2fcf7
Sharon Arana <shar_mdza@hotmail.com>
2023-03-13 20:15:38 -0600

    uso de user ref + tipos

--- src/components/RandomFox.tsx
+++ src/components/RandomFox.tsx
@@ -1,3 +1,5 @@
+import { useRef } from "react";
+
 interface Props {
   image: string;
   alt: string;
@@ -5,8 +7,11 @@ interface Props {
 
 //hay que dejar explícito que es un componente de react
 export const RandomFox = ({ image, alt }: Props): JSX.Element => {
+  const node = useRef<HTMLImageElement>(null); // se debe poner el elemento con el que se va a trabajar e inicializar en null cuando no sabemos el tipo
+
   return (
     <img
+      ref={node}
       width={320}
       height="auto"
       className="mx-auto rounded-md bg-gray-300"
