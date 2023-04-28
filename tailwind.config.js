21baa0378f7290f521a421fb10ccc360a4f16950
Sharon Arana <shar_mdza@hotmail.com>
2023-04-28 09:30:55 -0600

    Correcciones a configuracion de tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,27 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: { extend: {} },
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    colors: {
+      purple: "#948FCB",
+      lightPurple: "#F4F1FA",
+      black: "#3D3C41",
+      darkGray: "#3D3D3D",
+      gray: "#787878",
+      lightGray: "#C9C9C9",
+      pink: "#EA9A97",
+      white: "#FAFAFA",
+      red: "#E8466D",
+      notes: {
+        pink: "#EA9A97",
+        lightBlue: "#9BC1D9",
+        darkBlue: "#5C6B9D",
+        orange: "#F1B486",
+        purple: "#AAAED3",
+      },
+    },
+    extend: {},
+  },
+  // darkMode: "class",
   plugins: [],
 };
